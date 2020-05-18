4fc9421f93db130e44a6fce14ceaaf6e94638ec6
JeremyLampron <jeremy.lampron.01@edu.cegeptr.qc.ca>
2020-05-18 16:02:21 -0400

    Modif voyage possible par admin uniquement

--- View/Voyages/index.php
+++ View/Voyages/index.php
@@ -57,12 +57,13 @@ $voyages = get('voyages');
                     ?>
                     <?php
                     if ($connectedUser->getType() == 'admin' || $connectedUser->getType() == 'prof') {
+                        if ($connectedUser->getType() == 'admin'){
                         echo nav1(
                             '<img alt="modifier icon" src="Ressource/img/writing.png" class="images" data-toggle="tooltip" data-placement = "top" title = "Modifier">',
                             'Voyages',
                             'Edit',
                             $voyage->getIdVoyage());
-
+                              }
                         echo nav1(
                             '<img alt="invitation icon" src="Ressource/img/invite.png" class="images" data-toggle="tooltip" data-placement = "top" title = "Code d\'activation">',
                             'Activations',
@@ -127,5 +128,3 @@ $voyages = get('voyages');
     var order = [[ 7, 'desc' ],[ 5, 'asc' ],[ 0, 'asc' ]];
 </script>
 <?= load_script('paginator') ?>
-
-
