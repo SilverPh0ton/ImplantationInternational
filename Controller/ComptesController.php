658853d173be2823ff66a48f0bdf5808fe36cf59
Anthony <anthony.rompre.01@edu.cegeptr.qc.ca>
2020-05-19 11:36:14 -0400

    Update ComptesController.php
    verif age fonction edit comptesController

--- Controller/ComptesController.php
+++ Controller/ComptesController.php
@@ -186,6 +186,16 @@ class ComptesController extends AppController
                 }
 
                 $date_naissance = date("Y-m-d", strtotime($date_naissance_str));
+                $age = date_diff(date_create($date_naissance), new DateTime());
+
+                if ($age->y < 15) {
+                    $this->flashBad('La compte n\'a pas pu être modifié. La personne doit être agée d\'au moins 15 ans.');
+                    return $this->redirectParam1('Comptes', 'Edit', $id);
+                }
+                elseif ($age->y >= 90) {
+                    $this->flashBad('La compte n\'a pas pu être modifié. La personne ne peut être agée de plus de 90 ans.');
+                    return $this->redirectParam1('Comptes', 'Edit', $id);
+                }
 
                 $compte->setDateNaissance($date_naissance);
                 $compte->setTelephone($_POST['telephone']);
