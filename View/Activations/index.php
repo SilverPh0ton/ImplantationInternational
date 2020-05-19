5c1d49317fa4219b5aba616b8c782e0cf2f22678
Michel-Antoine Lemire <michel-antoine@outlook.com>
2020-05-19 11:54:23 -0400

    Move add btn to top right

--- View/Activations/index.php
+++ View/Activations/index.php
@@ -26,7 +26,7 @@ $activations = get('acti_array');
 
     <h3>Codes d'activation pour: <?= $nom_projet ?></h3>
 
-
+    <?=  nav1('<button class="add-btn">Générer des codes </button>','Activations','add',$id_voyage); ?>
 
     <?php if (is_array($activations) && sizeof($activations) != 0) : ?>
 
@@ -56,7 +56,6 @@ $activations = get('acti_array');
     <?php endif ?>
 
     <!--Button de navigation -->
-    <?=  nav1('<button>Générer des codes </button>','Activations','add',$id_voyage); ?>
     <?=  nav('<button>Revenir à la liste des voyages</button>','Voyages','index'); ?>
 
 </div>
